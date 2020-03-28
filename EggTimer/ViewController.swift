149d666ff9b3a32e261c649ed45f8c24ea5bb9c8
hnatmira <hnatmira@gmail.com>
2020-03-28 09:07:01 +0100

    switch for timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
+    //MARK: IBOutlet
+
+    var typeOfEgg = ["Soft": 7, "Medium": 10, "Hard": 12]
 
 
+    //MARK: IBAction
+    @IBAction func keyPressed(_ sender: UIButton) {
+
+        let hardness = sender.currentTitle!
+
+        switch hardness {
+        case "Soft":
+            print(typeOfEgg["Soft"]!)
+        case "Medium":
+            print(typeOfEgg["Medium"]!)
+        case "Hard":
+            print(typeOfEgg["Hard"]!)
+        default:
+            print("Error message")
+        }
+    }
 }
