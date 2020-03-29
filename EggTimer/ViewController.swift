11c06f9ff412764a8639eed61ba65a5caf951eae
hnatmira <hnatmira@gmail.com>
2020-03-29 20:09:54 +0200

    timer for progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,24 +11,39 @@ import UIKit
 class ViewController: UIViewController {
 
     //MARK: IBOutlet
+    @IBOutlet weak var countDownLabel: UILabel!
 
-    var typeOfEgg = ["Soft": 7, "Medium": 10, "Hard": 12]
+    var counter = 60
+    var timer = Timer()
 
+    func timeCalculation(timeInMinutes: Int) -> Int{
+        return timeInMinutes * 60
+    }
+
+    //MARK: Declaration
+    var eggTimes = ["Soft": 7, "Medium": 10, "Hard": 12]
 
     //MARK: IBAction
     @IBAction func keyPressed(_ sender: UIButton) {
 
-        let hardness = sender.currentTitle!
+        timer.invalidate()
 
-        switch hardness {
-        case "Soft":
-            print(typeOfEgg["Soft"]!)
-        case "Medium":
-            print(typeOfEgg["Medium"]!)
-        case "Hard":
-            print(typeOfEgg["Hard"]!)
-        default:
-            print("Error message")
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        counter = timeCalculation(timeInMinutes: eggTimes[hardness]!)
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateCounter() {
+        if counter > 0 {
+            print("\(counter) seconds.")
+            countDownLabel.text = String("\(counter) sec")
+            counter -= 1
+        } else {
+            timer.invalidate()
+            countDownLabel.text = "DONE"
         }
     }
 }
+
+
